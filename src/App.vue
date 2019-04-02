9f4e9413ffea67006577d17f0b94f32da611c224
yanh <891367701@qq.com>
2019-04-02 16:02:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <NavView></NavView>
+    <HomeView></HomeView>
+    <FootView></FootView>
+  </div>
+</template>
+<script>
+import NavView from './components/Nav.vue'
+import HomeView from './components/Home.vue'
+import FootView from './components/Footer.vue'
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    NavView,
+    HomeView,
+    FootView
+  }
+}
+
+</script>
+<style>
+.slide-up-enter-active,
+.slide-up-leave-active {
+  transition: all .4s cubic-bezier(0, 1.2, 1, 0.5);
+  opacity: .7;
+  transform: translate3d(0, 4em, 0);
+}
+
+.slide-up-enter,
+.slide-up-leave-active {
+  opacity: .3;
+  transform: translate3d(0, 4em, 0);
+}
+
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: all .4s cubic-bezier(0, 1.2, 1, 0.5);
+  opacity: .7;
+  transform: translate3d(0, 6em, 0);
+}
+
+.slide-down-enter,
+.slide-down-leave-active {
+  opacity: .1;
+  transform: translate3d(0, 6em, 0);
+}
+
+.slide-left-enter-active,
+.slide-left-leave-active {
+  transition: all .2s cubic-bezier(0, 1.2, 1, 0.5);
+  opacity: .5;
+  transform: translate3d(2em, 0, 0);
+}
+
+.slide-left-enter,
+.slide-left-leave-active {
+  opacity: .3;
+  transform: translate3d(2em, 0, 0);
+}
+
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: all .4s cubic-bezier(0, 1.2, 1, 0.5);
+  opacity: .5;
+  transform: translate3d(5em, 0, 0);
+}
+
+.slide-right-enter,
+.slide-right-leave-active {
+  opacity: .3;
+  transform: translate3d(5em, 0, 0);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .4s
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+require('./assets/css/base.css'); //引入全局的base文件
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
