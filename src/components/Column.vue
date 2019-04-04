00e5dc50e3774d6a1338ffb044c7cf5e4716b644
yanh <891367701@qq.com>
2019-04-04 21:28:38 +0800

    201904042130

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -2,64 +2,10 @@
     <div class="content mt">
       <div class="newsList">
         <ul>
-          <li>
+         <li v-for="(item,index) in list" :key="index">
             <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
-            </a>
-          </li>
-          <li>
-            <a href="">
-            	<h2>水电费水电费</h2>
-              <p>测试测是测是的范德萨发生的范德萨发斯蒂芬斯蒂芬的说法是的范德萨发斯蒂芬斯蒂芬</p>
+            	<h2>{{index+1}} .{{item.title}}</h2>
+              <p>{{item.detail}}</p>
             </a>
           </li>
         </ul>
@@ -68,8 +14,24 @@
 </template>
 <script>
 export default {
-
-
+  data(){
+    return {
+      list:[]
+    }
+  },
+  mounted(){
+     this.fetchdata();
+  },
+  methods:{
+    fetchdata(){
+        var _this=this;
+				this.$http.get('src/data/index.data').then(function(res){
+						_this.list=res.data;
+				}).catch(function(err){
+					console.log(err);
+				});
+    }
+  }
 }
 
 </script>
